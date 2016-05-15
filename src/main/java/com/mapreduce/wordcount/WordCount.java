5a4c2ac14d6dfd23284a08d716b45af80b6bdf6d
Prabhakaran Thatchinamoorthy <pthatchinamoorthy@homeaway.com>
2016-05-14 21:54:07 -0500

    Edited the Job name

--- src/main/java/com/mapreduce/wordcount/WordCount.java
+++ src/main/java/com/mapreduce/wordcount/WordCount.java
@@ -48,7 +48,7 @@ public class WordCount {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
+    Job job = Job.getInstance(conf, "wordcount");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
